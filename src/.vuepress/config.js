9c0107df85301b2da9890df7eb538f93e3aeed04
lvqq <nicksonlvqq@gmail.com>
2020-04-12 01:16:44 +0800

    feat: update README

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -11,7 +11,6 @@ module.exports = {
   ],
   plugins: [
     'vuepress-plugin-click',
-    'vuepress-plugin-cat',
     ['@vuepress/back-to-top', false],
     'vuepress-plugin-rocket',
     ['vuepress-plugin-zooming', {
